14e0e6f589fb7ec8e69cd922f32b5fa144668d8d
Jenny Soetanto <jenny.filcheck@gmail.com>
2019-11-16 16:31:30 -0600

    Setup GraphQL data example

--- src/server/services/graphql/schema.js
+++ src/server/services/graphql/schema.js
@@ -2,6 +2,21 @@ const typeDefinitions = `
   type Post {
     id: Int
     text: String
+    user: User
+  }
+
+  type User {
+    avatar: String
+    username: String
+  }
+
+  input PostInput {
+    text: String!
+  }
+
+  input UserInput {
+    username: String!
+    avatar: String!
   }
 
   type RootQuery {
@@ -9,8 +24,13 @@ const typeDefinitions = `
     posts: [Post]
   }
 
+  type RootMutation {
+    addPost(post: PostInput! user: UserInput!): Post
+  }
+
   schema {
     query: RootQuery
+    mutation: RootMutation
   }
 `
 
